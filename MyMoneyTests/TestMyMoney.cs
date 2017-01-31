60eec8f8ae2fab8e90fd96501c3f82f029f488cb
user <user@MEGAPC>
2017-01-31 15:53:03 +0300

    Добавьте файлы проекта.

---
+++ MyMoneyTests/TestMyMoney.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyMoneyTests
+{
+    [TestClass]
+    public class TestMyMoney
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
